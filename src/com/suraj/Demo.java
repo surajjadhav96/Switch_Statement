97ea01ea4cd83ad2af8a8133d2edb09d4cd8b92a
surajjadhav96 <sdjadhav420@gmail.com>
2022-12-06 01:25:50 +0530

    code aded

---
+++ src/com/suraj/Demo.java
@@ -0,0 +1,28 @@
+package com.suraj;
+
+public class Demo {
+
+	public static void main(String[] args) {
+String fruit="Apple";
+
+switch(fruit){
+case "mango":
+	System.out.println("fruit is Mango");
+	break;
+	
+case "Custard":
+	
+	System.out.println("fruit is Custard");
+	break;
+	
+case "Apple":
+	
+	System.out.println("fruit is Apple");
+	break;
+	
+	default:
+		System.out.println("out of fruit ");
+}
+	}
+
+}
